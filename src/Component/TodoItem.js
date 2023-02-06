306c6e9ec86f90487b2f5d511215908093b78e0d
rissotanuj <risso.tanuj@gmail.com>
2023-02-06 13:44:18 +0530

    Nothing

--- src/Component/TodoItem.js
+++ src/Component/TodoItem.js
@@ -5,7 +5,7 @@ export default function TodoItem({todo, OnDelete}) {
   return (
     <>
     <div>
-      <h6> Sno. {todo.sno+1}</h6>
+      <h6> Sno. {todo.sno +1 }</h6>
       <h4>{todo.Title}</h4> 
       <p>{todo.Desc}</p>
       <button className='btn btn-sm btn-danger' onClick={()=>{OnDelete(todo)}}>Delete</button>
